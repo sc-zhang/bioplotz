1b9dfff6f0b687877af460b45dad901e764a0ba3
zero <3406985348@qq.com>
2020-04-04 23:26:28 +0800

    add parameter

--- bioplot/manhattan.py
+++ bioplot/manhattan.py
@@ -8,4 +8,4 @@ __all__ = ["manhattan"]
 
 
 class _Manhattan(object):
-	def __init__(self, data, threshold=0.05, value_type="p-value", log_scale=2, plus_minus=False, xticklabels=True, yticklabels=True):
+	def __init__(self, data, threshold=0.05, value_type="p-value", color=['orange', 'green'], log_scale=2, plus_minus=False, xticklabels=True, yticklabels=True):
