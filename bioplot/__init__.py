a6c60f6af81076f48d3a8bcfcc5bb2b5e1131d63
sc-zhang <zsc-zhang@foxmail.com>
2023-04-25 09:41:23 +0800

    modify entry point

---
+++ bioplot/__init__.py
@@ -0,0 +1,8 @@
+import bioplot.chromosome
+import bioplot.genecluster
+import bioplot.manhattan
+
+
+chromosome = bioplot.chromosome.chromosome
+genecluster = bioplot.genecluster.gene_cluster
+manhattan = bioplot.manhattan.manhattan
