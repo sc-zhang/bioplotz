81a788c39a9093e6122a8ecc44c6760328fba8eb
sc-zhang <zsc-zhang@foxmail.com>
2023-04-25 16:15:24 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bioplotz',
+    version='0.1.0.dev0',
+    packages=['bioplotz'],
+    url='https://github.com/sc-zhang/bioplotz',
+    license='',
+    author='Shengcheng Zhang',
+    author_email='',
+    description='A python package for drawing some bioinformatic pictures.',
+    install_requires=['numpy',
+                      'matplotlib',
+                      'pandas']
+)
