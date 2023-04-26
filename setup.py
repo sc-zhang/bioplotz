ac2682b8d6aa2d2468f8363f1816c0a5375afe0a
sc-zhang <zsc-zhang@foxmail.com>
2023-04-26 12:27:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='bioplotz',
     version='0.1.0.dev0',
@@ -7,8 +11,10 @@ setup(
     url='https://github.com/sc-zhang/bioplotz',
     license='',
     author='Shengcheng Zhang',
-    author_email='',
+    author_email='zsc-zhang@foxmail.com',
     description='A python package for drawing some bioinformatic pictures.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['numpy',
                       'matplotlib',
                       'pandas']
