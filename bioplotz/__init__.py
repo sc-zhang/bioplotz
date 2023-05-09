e4e17b17e813ed7f6f596a43e9c434465996b311
sc-zhang <zsc-zhang@foxmail.com>
2023-05-09 17:42:51 +0800

    fix bug for multialign plot

--- bioplotz/__init__.py
+++ bioplotz/__init__.py
@@ -1,7 +1,7 @@
 from .chromosome import chromosome as chromosome
 from .genecluster import genecluster as genecluster
 from .manhattan import manhattan as manhattan
-from .mulitalign import multialign as multialign
+from .multialign import multialign as multialign
 
 
-__version__ = "0.1.0.dev5"
+__version__ = "0.1.0.dev6"
