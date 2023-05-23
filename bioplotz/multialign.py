3503801504bfb4afb3a51bc2c6efdd8d2012e484
sc-zhang <zsc-zhang@foxmail.com>
2023-05-23 11:51:08 +0800

    Allow setting background color

--- bioplotz/multialign.py
+++ bioplotz/multialign.py
@@ -6,23 +6,29 @@ class _MultiAlign(object):
 
     def __init__(self,
                  data: dict,
-                 match_color: any = 'blue',
-                 mismatch_color: any = 'red',
                  base_per_line: int = 80,
+                 match_color: any = 'blue',
+                 match_background_color: any = None,
+                 mismatch_color: any = 'red',
+                 mismatch_background_color: any = None,
                  highlight_positions: list = None,
-                 highlight_color: any = 'green'):
+                 highlight_color: any = 'green',
+                 highlight_background_color: any = None):
         self.__data = data
-        self.__match_color = match_color
-        self.__mismatch_color = mismatch_color
         self.__base_per_line = base_per_line
+        self.__match_color = match_color
+        self.__match_background_color = match_background_color
+        self.__mismatch_color = mismatch_color
+        self.__mismatch_background_color = mismatch_background_color
         if highlight_positions:
             self.__highlight_positions = set(highlight_positions)
         else:
             self.__highlight_positions = None
         self.__highlight_color = highlight_color
+        self.__highlight_background_color = highlight_background_color
 
     @staticmethod
-    def __rainbow_text(x, y, strings, colors, ax=None, **kwargs):
+    def __rainbow_text(x, y, strings, colors, background_colors, ax=None, **kwargs):
         """
         Take a list of *strings* and *colors* and place them next to each
         other, with text strings[i] being shown in colors[i].
@@ -47,8 +53,13 @@ class _MultiAlign(object):
         fig = ax.figure
         canvas = fig.canvas
 
-        for s, c in zip(strings, colors):
-            text = ax.text(x, y, s, color=c, transform=t, **kwargs)
+        for s, c, bc in zip(strings, colors, background_colors):
+            if bc:
+                text = ax.text(x, y, s, color=c,
+                               bbox=dict(facecolor=bc, linewidth=0, pad=0),
+                               transform=t, **kwargs)
+            else:
+                text = ax.text(x, y, s, color=c, transform=t, **kwargs)
 
             # Need to draw to update the text position.
             text.draw(canvas.get_renderer())
@@ -65,13 +76,13 @@ class _MultiAlign(object):
             break
         seq_cnt = len(self.__data)
         seq_list = sorted(self.__data.keys())
-        row_cnt = int(aln_len*1./self.__base_per_line)
+        row_cnt = int(aln_len * 1. / self.__base_per_line)
         if row_cnt * self.__base_per_line < aln_len:
             row_cnt += 1
         total_row_cnt = (row_cnt + 2) * seq_cnt
 
-        divide_repeat_cnt = int(self.__base_per_line/5.)
-        if divide_repeat_cnt*5 < self.__base_per_line:
+        divide_repeat_cnt = int(self.__base_per_line / 5.)
+        if divide_repeat_cnt * 5 < self.__base_per_line:
             divide_repeat_cnt += 1
 
         divide = '----+' * divide_repeat_cnt
@@ -85,8 +96,10 @@ class _MultiAlign(object):
 
             divide_and_pos = "%s %d-%d" % (divide, sp + 1, ep)
             colors = ['black' for _ in range(len(divide_and_pos))]
-            self.__rainbow_text(0, i * (seq_cnt + 2), list(divide_and_pos), colors, **kwargs)
+            background_colors = ['white' for _ in range(len(divide_and_pos))]
+            self.__rainbow_text(0, i * (seq_cnt + 2), list(divide_and_pos), colors, background_colors, **kwargs)
             colors = [self.__match_color for _ in range(ep - sp)]
+            background_colors = [self.__match_background_color for _ in range(ep - sp)]
 
             for k in range(sp, ep):
                 cur_set = set()
@@ -94,13 +107,16 @@ class _MultiAlign(object):
                     cur_set.add(self.__data[j][k])
                 if len(cur_set) > 1:
                     colors[k - sp] = self.__mismatch_color
+                    background_colors[k - sp] = self.__mismatch_background_color
                 if self.__highlight_positions and k in self.__highlight_positions:
                     colors[k - sp] = self.__highlight_color
+                    background_colors[k - sp] = self.__highlight_background_color
             for j in range(seq_cnt):
                 gid = seq_list[j]
                 y_ticks.append(i * (seq_cnt + 2) + j + 1)
                 y_labels.append(gid)
-                self.__rainbow_text(0, i * (seq_cnt + 2) + j + 1, self.__data[gid][sp: ep], colors, **kwargs)
+                self.__rainbow_text(0, i * (seq_cnt + 2) + j + 1, self.__data[gid][sp: ep],
+                                    colors, background_colors, **kwargs)
         ax.set_ylim(0, total_row_cnt)
         ax.set_xticks([])
         ax.invert_yaxis()
@@ -111,14 +127,24 @@ class _MultiAlign(object):
 
 
 def multialign(data: dict,
-               match_color: any = 'blue',
-               mismatch_color: any = 'red',
                base_per_line: int = 80,
+               match_color: any = 'blue',
+               match_background_color: any = None,
+               mismatch_color: any = 'red',
+               mismatch_background_color: any = None,
                highlight_positions: list = None,
                highlight_color: any = 'green',
+               highlight_background_color: any = None,
                **kwargs):
-
-    plotter = _MultiAlign(data, match_color, mismatch_color, base_per_line, highlight_positions, highlight_color)
+    plotter = _MultiAlign(data,
+                          base_per_line,
+                          match_color,
+                          match_background_color,
+                          mismatch_color,
+                          mismatch_background_color,
+                          highlight_positions,
+                          highlight_color,
+                          highlight_background_color)
 
     if not plt:
         plt.figure()
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='bioplotz',
-    version='0.1.0.dev6',
+    version='0.1.0.dev7',
     packages=['bioplotz'],
     url='https://github.com/sc-zhang/bioplotz',
     license='',
